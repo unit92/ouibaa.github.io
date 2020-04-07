f338a46539bd185da9daeb98a114a5b15233aa52
Weber Liu <54257955+weberliuMD@users.noreply.github.com>
2020-04-07 17:10:16 +1000

    changed to heart

--- pages/educational_resources/biodigital_meds1001/meds1001_anatomy/src/App.js
+++ pages/educational_resources/biodigital_meds1001/meds1001_anatomy/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 
 function App() {
-  var source = "https://human.biodigital.com/widget/?m=cochlear_implant&dk=" + "d35f19e377d3f4ecff9cb2abce7e2441032108c0"
+  var source = "https://human.biodigital.com/viewer/?be=3RFq&ui-info=true&ui-search=true&ui-reset=true&ui-fullscreen=true&ui-nav=true&ui-tools=true&ui-help=true&ui-chapter-list=false&ui-label-list=true&ui-anatomy-descriptions=false&ui-tutorial=false&disable-scroll=false&dk=d35f19e377d3f4ecff9cb2abce7e2441032108c0"
+
 
   return (
     <div className="App">
