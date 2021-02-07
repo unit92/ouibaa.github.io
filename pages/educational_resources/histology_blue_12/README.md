9f8c95f447953675c9a1a1643ba5e994e586f610
weberliu1996 <53137892+weberliu1996@users.noreply.github.com>
2021-02-08 09:54:48 +1100

    Added Histo Blue 12 slide demo

---
+++ pages/educational_resources/histology_blue_12/README.md
@@ -0,0 +1,2 @@
+# Histology Blue-12 Demo
+ Demonstration client-side ONLY application of Blue-12 Histology slide. Permission to host obtained from Dr. Suzanne Ollerenshaw at the University Of Sydney Department of Anatomy and Histology. This application is for demonstration purposes only for the development of a fuller-featured full-stack histology application. Application has been built with OpenSeaDragon.
---
+++ pages/educational_resources/histology_blue_12/slidedata.js
@@ -0,0 +1,24 @@
+slideData =
+  {
+    slideName: "BLUE-12",
+    imgLoc: "./blue-12/blue-0.xml",
+    videoLoc: "./Assets/video.mp4",
+    slideDescription:
+      "<b>Blue-12 Oesophagus</b> <br/> " +
+      "This is histological slide of the kidney. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    slideMetadata: {
+      Author: "Weber Liu",
+      Presenter: "Weber Liu",
+      Editor: "Weber Liu",
+      Copyright: "Slide copyright University of Sydney"
+    },
+    slideTimings: {
+      0: "70, 0.52, 0.84, 0.55",
+      1: "100, 0.36, 0.78, 8.22",
+      2: "185, 0.31, 0.78, 8.22",
+      3: "220, 0.23, 0.85, 4.11",
+      4: "321, 0.15, 0.84, 8.22",
+      5: "373, 0.52, 0.84, 0.55"
+    }
+  }
+  ;
