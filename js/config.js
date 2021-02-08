8c5c3ba608d77c193fc173efd261c5b32a515c7f
Jim Cook <jim.cook@sydney.edu.au>
2021-02-08 15:32:29 +1100

    added idam

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: ' ap-southeast-2_iIeIUekWT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ecph4abkl43k3md05i6sh1lm2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
