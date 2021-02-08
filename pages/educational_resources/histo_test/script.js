03b50214436ba1bcab99c7959de5b5e82ba8d9c5
weberliu1996 <53137892+weberliu1996@users.noreply.github.com>
2021-02-08 12:31:34 +1100

    updated with Proof of Concept Condyloma Acuminatum H+E stain
    for Babak

---
+++ pages/educational_resources/histo_test/script.js
@@ -0,0 +1,59 @@
+function loadSlide() {
+  viewer.open(slideData.imgLoc);
+  video.src = slideData.videoLoc;
+  document.getElementById("slideInfo").innerHTML = slideData.slideDescription;
+  console.log("Successfully loaded new slide");
+};
+
+function slidePosition() {
+  timingList.map(item => {
+    var timeData = item[1].split(",").map(Number)
+    // console.log(timeData)
+    console.log(this.currentTime)
+    if (Math.abs(this.currentTime - timeData[0]) < 1.5) {
+      moveViewPort(timeData[1], timeData[2], timeData[3]);
+    }
+  })
+}
+
+// do not change anything after this
+
+var viewer = OpenSeadragon({
+  id: "openseadragon",
+  prefixUrl: "./openseadragon-bin-2.4.1/images/",
+  showNavigator: true,
+  tileSources: "./test.dzi"
+});
+var viewPort = viewer.viewport;
+
+window.onload = function () {
+  video = document.createElement("video");
+  video.setAttribute("id", "videoPlayer");
+  video.setAttribute("controls", "controls");
+  video.addEventListener("timeupdate", slidePosition, false);
+  document.getElementById("slideVideo").appendChild(video);
+  loadSlide();
+};
+
+function moveViewPort(posX, posY, zoomDeg) {
+  viewPort.panTo(new OpenSeadragon.Point(posX, posY));
+  viewPort.zoomTo(zoomDeg);
+}
+
+console.log(slideData);
+var timingData = slideData['slideTimings'][0].split("&").map(Number)
+//function to output points on the slide
+var update_points = setInterval(function () {
+  console.log(
+    "(x, y, zoom) is " +
+    Math.round(viewPort.getCenter().x * 100) / 100 +
+    ", " +
+    Math.round(viewPort.getCenter().y * 100) / 100 +
+    ", " +
+    Math.round(viewPort.getZoom() * 100) / 100
+  );
+}, 3000);
+
+var timingList = Object.keys(slideData.slideTimings).map(function (key) {
+  return [Number(key), slideData.slideTimings[key]];
+});
---
+++ pages/educational_resources/histo_test/slidedata.js
@@ -0,0 +1,24 @@
+slideData =
+  {
+    slideName: "Condyloma Acuminatum HE",
+    imgLoc: "./test.dzi",
+    videoLoc: "",
+    slideDescription:
+      "<b>Condyloma Acuminatum HE</b> <br/> " +
+      "CA H+E Stain",
+    slideMetadata: {
+      Author: "Weber Liu",
+      Presenter: "Weber Liu",
+      Editor: "Weber Liu",
+      Copyright: "Slide copyright University of Sydney"
+    },
+    slideTimings: {
+      0: "70, 0.52, 0.84, 0.55",
+      1: "100, 0.36, 0.78, 8.22",
+      2: "185, 0.31, 0.78, 8.22",
+      3: "220, 0.23, 0.85, 4.11",
+      4: "321, 0.15, 0.84, 8.22",
+      5: "373, 0.52, 0.84, 0.55"
+    }
+  }
+  ;
